bd2f5e81a71dbf9f1f7986cd8cfa9799f861b780
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-29 23:11:04 +0600

    add dynamic params and display info ui

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Link from 'next/link'
 
 const index = ({ users }) => {
     return (
         <div>
-            <h1>Users size: {users.length}</h1>
+            <h1>Users: {users.length}</h1>
+            {
+                users.map(user => <div key={user.id}>
+                    <h3>Name: {user.name}
+                        <Link href={`/users/${user.id}`}>
+                            <button>Explore</button>
+                        </Link>
+                    </h3>
+                </div>)
+            }
         </div>
     );
 };
---
+++ pages/users/manager.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const manager = () => {
+    return (
+        <div>
+            <h1>This is Manager</h1>
+        </div>
+    );
+};
+
+export default manager;
